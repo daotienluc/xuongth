36c712fb2490188d3f6b35044893b54e55f18b17
daotienluc <lucd1177@gmail.com>
2024-06-12 09:55:23 +0700

    1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //xử lí chuyển trang trong phần trang chủ
+//đăng nhập
+function btndn() {
+  window.location.href = "./login/login.html";
+}
+
+AOS.init({
+  duration: 2000, // Thời gian mặc định cho tất cả các hiệu ứng là 2000ms
+  once: true, // Hiệu ứng chỉ chạy một lần khi lướt trang xuống
+});
